698e7d73ca31dc2e96213554fa6b8fc01451f3d0
HyeongMin Kim <92067099+fkgnssla@users.noreply.github.com>
2022-08-31 16:12:09 +0900

    [API 개발 고급 - 지연 로딩과 조회 성능 최적화]: 주문 조회 V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,12 +1,17 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -29,4 +34,35 @@ public class OrderSimpleApiController {
         }
         return all;
     }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        //N+1 문제 (1: order 조회, N: Member 조회,Delivery 조회)
+        //현재 메서드에서는 쿼리가 1+N+N번 실행된다. (order 조회 1번) + (M,D 조회(userA)) + (M,D 조회(userB)) => 1+2+2 => 5번
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> simpleOrderDtos = new ArrayList<>();
+        for(Order order : orders) {
+            simpleOrderDtos.add(new SimpleOrderDto(order));
+        }
+
+        return simpleOrderDtos;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name; //주문자
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //Lazy 초기화
+        }
+    }
+
 }
